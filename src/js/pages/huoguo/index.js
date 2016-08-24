8c6a714fbb3908bb66f92b1ceb04f2403b21db89
sea13502 <sea13502@163.com>
2016-08-24 15:24:22 +0800

    雏形分模板，可以在项目入口里面读取接口判断是哪个业态，然后加载不同的模板，得是按需加载的

---
+++ src/js/pages/huoguo/index.js
@@ -0,0 +1,11 @@
+var React = require( "react" );
+
+var Huoguo = React.createClass({
+	render:function(){
+		return (
+			<div>这是火锅的主题</div>
+		);
+	}
+});
+
+module.exports = Huoguo;
---
+++ src/js/pages/kuaican/index.js
@@ -0,0 +1,11 @@
+var React = require( "react" );
+
+var Huoguo = React.createClass({
+	render:function(){
+		return (
+			<div>这是快餐的主题</div>
+		);
+	}
+});
+
+module.exports = Huoguo;
