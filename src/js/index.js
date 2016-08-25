6b488335ec36f6711c25626fa5c3dd7146b0e44a
sea13502 <sea13502@163.com>
2016-08-25 11:56:01 +0800

    显示出了菜品

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,8 @@
 var React = require( "react" );
 var ReactDOM  = require( "react-dom" );
 var Huoguo = require( "./pages/kuaican/index.js" );
+var data = require( "./DiancaiExampleData.js" );
 
+data.init();
 
 ReactDOM.render( <Huoguo />,document.getElementById("test") );
--- src/js/pages/kuaican/index.js
+++ src/js/pages/kuaican/index.js
@@ -1,9 +1,36 @@
 var React = require( "react" );
+require('../../../assets/style/main.scss');
+//console.log( JSON.parse( localStorage.getItem('commonDish') ) );
+//console.log( JSON.parse( localStorage.getItem('tcDish') ) );
+
+var commonDish = JSON.parse( localStorage.getItem('commonDish') );
 
 var Huoguo = React.createClass({
 	render:function(){
+
+		var wholedish = [];
+		
+		for( var i = 0 ; i < commonDish.alldish.length ; i++ ){
+			var singleClassDish = [];
+			singleClassDish.push(
+				<div className="dishtitle">
+					{ commonDish.alldish[i].name }
+				</div>
+			);
+			for( var j = 0 ; j < commonDish.alldish[i].items.length ; j++ ){
+				singleClassDish.push(
+					<div>
+						{ commonDish.alldish[i].items[j].name }
+					</div>
+				);
+			}
+			wholedish.push( singleClassDish );
+		}
+
 		return (
-			<div>这是快餐的主题</div>
+			<div>
+				{ wholedish }
+			</div>
 		);
 	}
 });
